5e0204bb99f71ebbf928606c0611b3a590e04608
ddd-arc-v <83993981+ddd-arc-v@users.noreply.github.com>
2022-10-10 04:41:54 +0800

    Update status.dart

--- lib/status.dart
+++ lib/status.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
-class Status extends StatelessWidget {
-  const Status({super.key});
+class Pembayaran extends StatelessWidget {
+  const Pembayaran({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Status Antrian'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Halaman Status Antrian')
+      body: Container(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("Nama1"),
+              subtitle: Text("Deskripsi"),
+              trailing: Text("10.00 AM"),
+              onTap: () {},
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("Nama2"),
+              subtitle: Text("Deskripsi"),
+              trailing: Text("10.00 AM"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("Nama3"),
+              subtitle: Text("Deskripsi"),
+              trailing: Text("10.00 AM"),
+              onTap: () {},
+            )
           ],
         ),
       ),
