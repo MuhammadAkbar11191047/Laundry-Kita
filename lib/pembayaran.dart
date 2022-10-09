eb6eb25fb6feae881fbfcf0dbe860fb3955d2557
ddd-arc-v <83993981+ddd-arc-v@users.noreply.github.com>
2022-10-10 04:42:33 +0800

    Update pembayaran.dart

--- lib/pembayaran.dart
+++ lib/pembayaran.dart
@@ -6,14 +6,33 @@ class Pembayaran extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pembayaran'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Halaman Pembayaran')
+      body: Container(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("Nama1"),
+              subtitle: Text("Deskripsi"),
+              trailing: Text("10.00 AM"),
+              onTap: () {},
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("Nama2"),
+              subtitle: Text("Deskripsi"),
+              trailing: Text("10.00 AM"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("Nama3"),
+              subtitle: Text("Deskripsi"),
+              trailing: Text("10.00 AM"),
+              onTap: () {},
+            )
           ],
         ),
       ),
